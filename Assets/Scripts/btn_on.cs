ed91d723c2bc96e37282f5c61fbdd51f937d6822
Sdavi3489 <sdavi3489@gmail.com>
2023-06-18 15:30:07 -0300

    Commit2: Atualizando o sistema solar com as orbitas em 3D, adcionando planetas como saturno e urano com os aneis

---
+++ Assets/Scripts/btn_on.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class btn_on : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbButtonObject;
+    //public GameObject SolarSystem;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbButtonObject = GameObject.Find("btn_on");
+        //SolarSystem = GameObject.Find("sistema_solar_3d");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        animator.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Botão on pressionado");
+        animator.Play("none");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Botão released");
+        animator.Play("Take 001");
+        
+    }
+}
