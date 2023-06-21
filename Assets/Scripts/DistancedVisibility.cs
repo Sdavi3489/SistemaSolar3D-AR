5cb311f14657eaf072a020836d01ad0502ac7a63
Sdavi3489 <sdavi3489@gmail.com>
2023-06-21 01:25:34 -0300

    Implementando novo sistema solar e novas targets dos planetas

---
+++ Assets/Scripts/DistancedVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistancedVisibility : MonoBehaviour
+{
+    public Transform target; // Referência ao marcador de AR
+    public float maxDistance = 10f; // Distância máxima para exibir o objeto
+
+    private Renderer objectRenderer;
+
+    private void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        // Verificar a distância entre a câmera e o marcador de AR
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        // Ativar ou desativar o objeto 3D com base na distância
+        objectRenderer.enabled = distance <= maxDistance;
+    }
+}
---
+++ Assets/Scripts/Rotation_planets.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation_planets : MonoBehaviour
+{
+
+    public float rotationSpeed = 50f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
