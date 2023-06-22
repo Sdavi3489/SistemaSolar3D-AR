a1304bfd634347220a8af65f124d83450ac9d393
Sdavi3489 <sdavi3489@gmail.com>
2023-06-21 23:11:13 -0300

    Adicionando novo menu para a aplicacao e desenvolvimento de script para carregar cenas

---
+++ Assets/Scripts/loadScenesScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScenesScript : MonoBehaviour
+{
+    public void CarregarCena(string cena){
+        SceneManager.LoadScene(cena);
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
