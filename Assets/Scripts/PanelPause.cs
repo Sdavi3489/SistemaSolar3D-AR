ad30ef9811c86c6ab2783fe522c31389407842f2
Sdavi3489 <sdavi3489@gmail.com>
2023-06-23 00:10:33 -0300

    Desenvolvimento do menu de pause, adicionando scene de instrucoes, adicionando novas assets dos butoes.

---
+++ Assets/Scripts/PanelPause.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelPause : MonoBehaviour
+{
+    public GameObject pausePanel;
+    
+    public void Pause(){
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Continue(){
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
