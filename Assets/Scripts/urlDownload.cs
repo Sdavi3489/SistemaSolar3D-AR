4fee6a45a7184659d5f6950caf314acedb7cbe72
Sdavi3489 <sdavi3489@gmail.com>
2023-07-11 22:44:27 -0300

    Desenvolvimento da cena sobre, melhorias na tela de instrucoes e criacao de script para redirecionar para o pdf dos marcadores entre outras melhorias

---
+++ Assets/Scripts/urlDownload.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urlDownload : MonoBehaviour
+{
+    public void urlRedirect(){
+        Application.OpenURL("https://drive.google.com/drive/folders/1p00DUBXlTuqdelXifUs167iANq5nb4xl?usp=drive_link");
+    }
+}
